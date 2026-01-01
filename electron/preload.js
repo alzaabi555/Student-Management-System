8d711499a973604c80e8eb3727e2ed16ad4018af
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-01-02 02:41:43 +0400

    Add files via upload

--- electron/preload.js
+++ electron/preload.js
@@ -1,4 +1,4 @@
-const { contextBridge, shell } = require('electron');
+import { contextBridge, shell } from 'electron';
 
 contextBridge.exposeInMainWorld('electron', {
   openExternal: (url) => shell.openExternal(url)
