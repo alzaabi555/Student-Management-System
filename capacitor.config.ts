b54f3fc28c32221168526c939054d697231f2bd2
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-01-02 02:08:21 +0400

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.madrasati.app',
-  appName: 'مدرستي',
+  appId: 'com.madrasati.app.v2',
+  appName: 'مدرستي المطور',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
